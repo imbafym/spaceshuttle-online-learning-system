f713cc1802e8d3629ebfec1a0f33737f81d0d0cd
imbafym <707082902@qq.com>
2017-11-25 22:21:41 +1100

    halfway homework add userDetailsService and UserRepository and UserDetailImpl

---
+++ src/main/java/com/example/springsecurity/service/UserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.springsecurity.service;
+
+import com.example.springsecurity.model.User;
+import com.example.springsecurity.model.UserDetailsImpl;
+import com.example.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsService  implements org.springframework.security.core.userdetails.UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email)  {
+
+
+            User user = userRepository.findUserByEmail(email);
+            if(user == null){
+                throw new UsernameNotFoundException(email);
+            }
+
+
+        return new UserDetailsImpl(user);
+    }
+}
