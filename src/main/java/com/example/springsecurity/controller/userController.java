95f86599bcab5d5f6530d28bd187c1511fe0337f
imbafym <707082902@qq.com>
2017-12-02 18:32:52 +1100

    gradle update

--- src/main/java/com/example/springsecurity/controller/userController.java
+++ src/main/java/com/example/springsecurity/controller/userController.java
@@ -1,14 +1,13 @@
 package com.example.springsecurity.controller;
 
 
+
 import com.example.springsecurity.model.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
